b8adf1f8daa794254407ad38f4d61fb66115fe6e
SantiagoToloza <santiago.b.toloza@gmail.com>
2025-01-09 21:32:57 -0300

    Add: implement styles in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,13 @@ import { useSelector } from 'react-redux';
 const Navbar = () => {
     const cartItems = useSelector((state) => state.cart.items);
     const credit = useSelector((state) => state.credit.balance);
-    const profilename = useSelector((state) => state.profile.username);
+    const profilename = useSelector((state) => state.profile.firstName);
     return (
-        <nav className="bg-blue-500 p-4 text-white flex justify-between">
-            <Link to="/">Catálogo</Link>
+        <nav className="py-4 bg-primary text-white flex justify-between m-2 px-4">
+            <div>
+
+                <Link to="/" className='font-bold text-2xl'>Tienda de productos</Link>
+            </div>
             <div className="flex gap-4">
                 <span>{profilename}</span>
                 <Link to="/cart">Carrito ({cartItems.length})</Link>
--- src/pages/Catalago.jsx
+++ src/pages/Catalago.jsx
@@ -32,11 +32,11 @@ const Catalogo = () => {
     };
 
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Catálogo de Productos</h1>
+        <div className="p-4 mx-auto w-1/2 h-auto min-h-3/4 ">
+            <h1 className="text-3xl font-bold mb-4 text-center  ">Catálogo</h1>
 
 
-            <div className="flex gap-4 mb-4">
+            <div className="flex gap-4 mb-4  justify-between">
                 <input
                     type="text"
                     placeholder="Buscar por nombre..."
@@ -51,7 +51,7 @@ const Catalogo = () => {
                 </select>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-3/4">
                 {currentItems.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
