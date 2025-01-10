2ad918a697319b450f2d83e59546d0e60501724f
SantiagoToloza <santiago.b.toloza@gmail.com>
2025-01-10 18:38:59 -0300

    feat:  added redux-persist for persistence

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cart/cartSlide';
 import creditReducer from '../features/credit/creditSlice';
 import productsReducer from '../features/products/productSlide';
 import profileReducer from '../features/profile/profileSlide';
-export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        credit: creditReducer,
-        products: productsReducer,
-        profile: profileReducer,
-    },
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart', 'credit'],
+};
+
+const rootReducer = combineReducers({
+    credit: creditReducer,
+    cart: cartReducer,
+    products: productsReducer,
+    profile: profileReducer,
 });
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
